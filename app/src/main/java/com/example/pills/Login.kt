f0d4b1865409ea1ea1b5775224bcf771a8c463ea
Samuel Bautista <samuelbaubas@gmail.com>
2022-05-02 23:54:33 -0500

    se agrego un archivo nav.xml para a navegacion entre fragmentes y un nuevo icono para email, agregamos las dependecias de navegacion, terminamos el disenio de SignUp screen, de igual manera hicimos ajustes en la login Screen para algunas correcciones de nombres, agregamos los strings de la nueva pantalla al archivo strings.xml en values, tambien corrio el nav_graph.xml para ver el flujo de la app

--- app/src/main/java/com/example/pills/Login.kt
+++ app/src/main/java/com/example/pills/Login.kt
@@ -1,11 +1,22 @@
 package com.example.pills
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signUp()
+    }
+
+    private fun signUp() {
+        val goToSignUpButton: Button= findViewById(R.id.go_to_signup)
+        goToSignUpButton.setOnClickListener {
+            val signUp= Intent(this,SingUp::class.java)
+            startActivity(signUp)
+        }
     }
 }
